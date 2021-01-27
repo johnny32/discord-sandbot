cd041d6bf3bee2312219d2231426dde10eec13b8
Johnny Clara <jonathan.clara.marquez@gmail.com>
2021-01-27 17:52:39 +0100

    Init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace discord_sandbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
