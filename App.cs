93431c7d3c05b5b3ac322e439d2fc710543e93c4
Johnny Clara <jonathan.clara.marquez@gmail.com>
2021-01-27 18:35:36 +0100

    Configuration and Discord client tied up together

--- App.cs
+++ App.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
 using Microsoft.Extensions.Configuration;
 
 namespace DiscordSandbot
@@ -13,24 +15,22 @@ namespace DiscordSandbot
 
         public async Task RunAsync()
         {
-            await Task.Delay(-1);
-
-            /*var discord = new DiscordClient(new DiscordConfiguration()
+            var discord = new DiscordClient(new DiscordConfiguration()
             {
-                Token = "MY TOKEN",
+                Token = _configuration.DiscordToken,
                 TokenType = TokenType.Bot
             });
 
             discord.MessageCreated += OnDiscordMessageCreated;
 
             await discord.ConnectAsync();
-            await Task.Delay(-1);*/
+            await Task.Delay(-1);
+        }
 
-            /*static async Task OnDiscordMessageCreated(MessageCreateEventArgs args)
-            {
+        static async Task OnDiscordMessageCreated(MessageCreateEventArgs args)
+        {
             if (args.Message.Content == "ping")
                 await args.Message.RespondAsync("pong");
-            }*/
         }
     }
 }
