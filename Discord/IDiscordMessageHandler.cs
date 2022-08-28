37cd55c7e691f0878dc96424064ebede846de0ae
Johnny Clara <jonathan.clara.marquez@gmail.com>
2022-08-28 11:54:08 +0200

    Added documentation to services public methods.

--- Discord/IDiscordMessageHandler.cs
+++ Discord/IDiscordMessageHandler.cs
@@ -6,8 +6,23 @@ namespace DiscordSandbot.Discord
 {
     public interface IDiscordMessageHandler
     {
+        /// <summary>
+        /// Registers the custom emojis for statistics when used in a message, replies with funny rhymes when possible, tells people to calm down and randomly insults and asks for forgiveness later.
+        /// </summary>
+        /// <param name="client">The Discord client to attach to the object.</param>
+        /// <param name="args">The message event arguments, containin information like who sent the message, the contents of the message itself, etc.</param>
         Task HandleMessageAsync(DiscordClient client, MessageCreateEventArgs args);
+        /// <summary>
+        /// Registers the custom emojis for statistics when added as a reaction to a message.
+        /// </summary>
+        /// <param name="client">The Discord client to attach to the object.</param>
+        /// <param name="args">The message event arguments, containin information like who sent the message, the contents of the message itself, etc.</param>
         Task HandleAddReactionAsync(DiscordClient client, MessageReactionAddEventArgs args);
+        /// <summary>
+        /// Deregisters the custom emojis for statistics when removed from a reaction to a message.
+        /// </summary>
+        /// <param name="client">The Discord client to attach to the object.</param>
+        /// <param name="args">The message event arguments, containin information like who sent the message, the contents of the message itself, etc.</param>
         Task HandleRemoveReactionAsync(DiscordClient client, MessageReactionRemoveEventArgs args);
     }
 }
--- HarterQuotes/IHarterQuotesService.cs
+++ HarterQuotes/IHarterQuotesService.cs
@@ -5,6 +5,10 @@ namespace DiscordSandbot.HarterQuotes
 {
     public interface IHarterQuotesService
     {
+        /// <summary>
+        /// Writes a random Harter quote when asked for with the command "!!Harter".
+        /// </summary>
+        /// <param name="context">The Discord command context to which the service will reply.</param>
         Task GetRandomQuoteAsync(CommandContext context);
     }
 }
